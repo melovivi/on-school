2aa52ada922532a53dd3417c93138ba2e33da892
viviani.rodrigues <viviani.rodrigues@elo.com.br>
2024-09-18 17:04:26 -0300

    Inclusao do find user

---
+++ src/http/controllers/user/find.ts
@@ -0,0 +1,18 @@
+import { ResourceNotFoundError } from "@/use-cases/errors/resource-not-found-error";
+import { makeSigninUseCase } from "@/use-cases/factory/make-signin-use-case";
+import { FastifyReply, FastifyRequest } from "fastify";
+
+export async function find(req: FastifyRequest, reply: FastifyReply) {
+    const reqUser = req.user as { email: string }
+
+    const signinUseCase = makeSigninUseCase()
+
+    const user = await signinUseCase.handler(reqUser.email)
+
+    if (!user) {
+        throw new ResourceNotFoundError('Post')
+      }
+    
+      return reply.status(200).send(user)
+    
+}
--- src/http/controllers/user/router.ts
+++ src/http/controllers/user/router.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from 'fastify'
 import { create } from './create'
 import { signin } from './signin'
+import { find } from './find'
 
 export async function userRoutes(app: FastifyInstance) {
   app.post('/user', create)
   app.post('/user/signin', signin)
+  app.get('/user/me', find)
 }
