bdb06b3ada709fa892bdbf23a7ed49a59810a258
viviani.rodrigues <viviani.rodrigues@elo.com.br>
2024-09-25 21:27:12 -0300

    Ajuste de use case

--- src/use-cases/search-post.ts
+++ src/use-cases/search-post.ts
@@ -4,7 +4,7 @@ import { IPostRepository } from '@/repositories/post.repository.interface'
 export class SearchPostUseCase {
   constructor(private postRepository: IPostRepository) {}
 
-  async handler(page: number, limit: number, keyword: string): Promise<Post[]> {
+  async handler(page: number, limit: number, keyword: string): Promise<{ data: Post[], pagination: { totalItems: number, totalPages: number, currentPage: number }}> {
     return this.postRepository.searchByWord(page, limit, keyword)
   }
 }
