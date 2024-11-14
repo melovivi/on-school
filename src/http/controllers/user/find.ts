bce7ce259661f59bdd2d84b86be0745c05626922
viviani.rodrigues <viviani.rodrigues@elo.com.br>
2024-11-14 16:37:20 -0300

    ajuste eslint

--- src/http/controllers/user/find.ts
+++ src/http/controllers/user/find.ts
@@ -1,18 +1,17 @@
-import { ResourceNotFoundError } from "@/use-cases/errors/resource-not-found-error";
-import { makeSigninUseCase } from "@/use-cases/factory/make-signin-use-case";
-import { FastifyReply, FastifyRequest } from "fastify";
+import { ResourceNotFoundError } from '@/use-cases/errors/resource-not-found-error'
+import { makeSigninUseCase } from '@/use-cases/factory/make-signin-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function find(req: FastifyRequest, reply: FastifyReply) {
-    const reqUser = req.user as { email: string }
+  const reqUser = req.user as { email: string }
 
-    const signinUseCase = makeSigninUseCase()
+  const signinUseCase = makeSigninUseCase()
 
-    const user = await signinUseCase.handler(reqUser.email)
+  const user = await signinUseCase.handler(reqUser.email)
 
-    if (!user) {
-        throw new ResourceNotFoundError('Post')
-      }
-    
-      return reply.status(200).send(user)
-    
+  if (!user) {
+    throw new ResourceNotFoundError('Post')
+  }
+
+  return reply.status(200).send(user)
 }
